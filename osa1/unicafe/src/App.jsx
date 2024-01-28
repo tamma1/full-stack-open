7d199e766b85a82bd6408dbd3d780f42426c290c
onnit <onni.tammi@aalto.fi>
2024-01-28 17:13:02 +0200

    unicafe 1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -14,13 +14,38 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Statistics = ({ good, neutral, bad}) => (
-  <div>
-    <p>good: {good}</p>
-    <p>neutral: {neutral}</p>
-    <p>bad: {bad}</p>
-  </div>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
+
+const Statistics = ({ good, neutral, bad}) => {
+  
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = `${100 * good / all} %`
+
+  if (all === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good}/>
+        <StatisticLine text='neutral' value={neutral}/>
+        <StatisticLine text='bad' value={bad}/>
+        <StatisticLine text='all' value={all}/>
+        <StatisticLine text='average' value={average}/>
+        <StatisticLine text='positive' value={positive}/>
+        </tbody>
+    </table>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
