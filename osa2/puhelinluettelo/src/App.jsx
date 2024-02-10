ac2b8e38a584290a7c58b72bd1c67f256fb3fa67
onnit <onni.tammi@aalto.fi>
2024-02-10 13:24:59 +0200

    2.10 kurssitiedot

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,11 +1,58 @@
 import { useState } from 'react'
 
+const Filter = ({ value, onChange }) => (
+  <form>
+    <div>
+      filter shown with <input
+      value={value}
+      onChange={onChange}
+      />
+    </div>
+  </form>
+)
+
+const PersonForm = ({ onSubmit, newName, newNumber, onNameChange, onNumberChange}) => (
+  <form onSubmit={onSubmit}>
+    <div>
+      name: <input 
+      value={newName}
+      onChange={onNameChange}
+      />
+    </div>
+    <div>
+      number: <input
+      value={newNumber}
+      onChange={onNumberChange}
+      />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map((person, index) =>
+      <div key={index}>
+        {person.name} {person.number}
+      </div>
+    )}
+  </div>
+)
+
 const App = () => {
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '1234' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,36 +77,33 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
+  const filteredPersons = filter
+    ? persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input 
-          value={newName}
-          onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number: <input
-          value={newNumber}
-          onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person, index) =>
-        <div key={index}>
-          {person.name} {person.number}
-        </div>
-      )}
-      
+      <Filter value={filter} onChange={handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm 
+        onSubmit={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      /> 
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons}/>
     </div>
   )
-
 }
 
 export default App
