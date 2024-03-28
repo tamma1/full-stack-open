2d44bbf2bea3ed6d081a7609a0140c84ed3d8f1c
onnit <onni.tammi@aalto.fi>
2024-03-28 12:03:10 +0200

    4.15 blogilista

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const mongoose = require('mongoose')
 require('express-async-errors')
 
@@ -12,6 +15,8 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
