3e65da384e6a20a6f95c358cea026e555f088f2d
onnit <onni.tammi@aalto.fi>
2024-09-02 11:04:48 +0300

    blogilista 5.17

---
+++ osa5/bloglist-tests/tests/bloglist.spec.js
@@ -0,0 +1,23 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('bloglist', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
