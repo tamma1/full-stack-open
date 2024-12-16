c6f624c7d6d0db21889c119b56f2d8eb35973ab0
onnit <onni.tammi@live.com>
2024-12-16 15:41:23 +0200

    6.9 paremmat anekdootit

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,8 +2,13 @@ import { useSelector, useDispatch } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => 
-    [...state].sort((a, b) => b.votes - a.votes))
+
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    const filtered = anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+    return [...filtered].sort((a, b) => b.votes - a.votes)
+  })
 
   const dispatch = useDispatch()
 
